8658fafe9d17f3b3f6549c18462e0f012549d2ae
Jonathan R. Wallace <jonathan.wallace@gmail.com>
2016-03-26 13:00:10 -0400

    Adds ultra for syntax highlighting in the repl

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,2 +1,3 @@
-;; installed for better vim support with vim-fireplace
-{ :user { :plugins [[cider/cider-nrepl "0.11.0"]] }}
+;; installed cider-nrepl for better vim support with vim-fireplace
+;; ultra makes the repl look pretty
+{:user { :plugins [[cider/cider-nrepl "0.11.0"] [venantius/ultra "0.4.1"]]}}
