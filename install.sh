51cde79b70b7db9ea93dbafb9f937db4fb8e0979
Jonathan R. Wallace <jonathan.wallace@gmail.com>
2021-10-01 11:03:15 -0400

    Testing out a codespaces setup

---
+++ install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+exec > >(tee -i $HOME/dotfiles_install.log)
+exec 2>&1
+set -x
+
+fancy_echo() {
+  local fmt="$1"; shift
+
+  # shellcheck disable=SC2059
+  printf "\\n$fmt\\n" "$@"
+}
+
+get() {
+  curl -fLo $1 --create-dirs $2
+}
+
+if [ "$CODESPACES" == "true" ]; then
+  fancy_echo "In codespaces! Installing dotfiles"
+  locals=( "tmux.conf.local" "vimrc.local" "vimrc.bundles.local" "aliases.local" "gitconfig.local" "ripgreprc" )
+  for i in "${locals[@]}"
+  do
+    ln -s $(pwd)/"$i" $HOME/."$i"
+  done
+
+  fancy_echo "Installing apt-get packages"
+  apt-get install fzf universal-ctags neovim
+
+  #fancy_echo "Getting thoughtbot dotfiles"
+  #get $HOME/.vimrc https://raw.githubusercontent.com/thoughtbot/dotfiles/master/vimrc
+  #get $HOME/.vimrc.bundles https://raw.githubusercontent.com/thoughtbot/dotfiles/master/vimrc.bundles
+  #get $HOME/.aliases https://raw.githubusercontent.com/thoughtbot/dotfiles/master/aliases
+  #get $HOME/.gitconfig https://raw.githubusercontent.com/thoughtbot/dotfiles/master/gitconfig
+  #get $HOME/.gitmessage https://raw.githubusercontent.com/thoughtbot/dotfiles/master/gitmessage
+  #get $HOME/.gitignore https://raw.githubusercontent.com/thoughtbot/dotfiles/master/gitignore
+  #get $HOME/.tmux.conf https://raw.githubusercontent.com/thoughtbot/dotfiles/master/tmux.conf
+
+  fancy_echo "Installing vim plugins"
+  if [ -e "$HOME"/.vim/autoload/plug.vim ]; then
+    vim -E -s +PlugUpgrade +qa
+  else
+    curl -fLo "$HOME"/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  fi
+  vim -u "$HOME"/.vimrc.bundles +PlugUpdate +PlugClean! +qa
+  reset -Q
+
+  #fancy_echo "Sourcing aliases"
+  #[[ -f ~/.aliases ]] && source ~/.aliases
+  #echo "alias g='git'" >> "$HOME"/.bashrc
+  #echo "export EDITOR=vim" >> "$HOME"/.bashrc
+
+  ##fancy_echo "Installing gems"
+  ##gem install ripper-tags && ripper-tags -R
+
+  fancy_echo "Sourcing bashrc"
+  source ~/.bashrc
+
+  fancy_echo "All done"
+else
+  fancy_echo "Not running in a codespace"
+fi
